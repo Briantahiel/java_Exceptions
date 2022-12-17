ed069f010b638caf1c597c4efdf855782fd73a14
BrianTahiel <briang.2012@hotmail.com>
2022-12-17 17:18:18 -0300

    minor fixes

--- Flow.java
+++ Flow.java
@@ -1,22 +1,30 @@
-public class Flow{
-    public static void main(String[] args){
+public class Flow {
+    public static void main(String[] args) {
         System.out.println("Start main");
         method1();
         System.out.println("End main");
     }
-    private static void method1(){
+
+    private static void method1() {
         System.out.println("Start method1");
         method2();
         System.out.println("End method1");
     }
+
     private static void method2(){
         System.out.println("Start method2");
         for(int i = 1; i <= 5; i++){
             System.out.println(i);
-            int num = 0;
-            int result = i/num;
-            System.out.println(result);
+            try{  
+                int num = 0;
+                int result = i/num;
+                System.out.println(result);
+            }catch(ArithmeticException exception){
+                System.out.println(exception.getMessage());
+                exception.printStackTrace();;
+            }
         }
         System.out.println("End method2");
     }
+    
 }
